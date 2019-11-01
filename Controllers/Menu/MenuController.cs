a6b4040ab4dc898d67aa19ef36313b35d473ae34
Bruno Tumelero <51979425+btumelero@users.noreply.github.com>
2019-11-01 14:06:23 -0300

    Add files via upload

---
+++ Controllers/Menu/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+  /*
+   * Troca pro menu de escolha da nave quando o usuário clicar no respectivo botão do menu
+   */
+  public void loadSpaceshipSelection () {
+    SceneManager.LoadScene("SpaceshipSelection");
+  }
+
+  /*
+   * Sai do jogo quando o usuário clicar no respectivo botão do menu
+   */
+  public void exitGame () {
+    Application.Quit();
+  }
+
+}
---
+++ Controllers/Menu/SpaceshipSelectionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SpaceshipSelectionController : MonoBehaviour {
+
+  public static GameObject spaceship;
+  public GameObject[] spaceshipsPrefabs;
+  private Dictionary<string, GameObject> spaceships;
+
+  private void Start () {
+    spaceships = new Dictionary<string, GameObject> {
+       { "PlayerAttacker", spaceshipsPrefabs[0] },
+       { "PlayerDefender", spaceshipsPrefabs[1] },
+       { "PlayerDodger", spaceshipsPrefabs[2] },
+       { "PlayerNormal", spaceshipsPrefabs[3] }
+    };
+  }
+
+  /*
+   * Troca pra fase de sobrevivência quando o usuário escolhe uma das naves
+   */
+  public void clickedButton () {
+    spaceship = spaceships["Player" + EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text];
+    SceneManager.LoadScene("Survival");
+  }
+
+  public void loadMenu () {
+    SceneManager.LoadScene("Menu");
+  }
+}
