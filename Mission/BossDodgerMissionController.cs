2619e806d807b3795a897d2536273e37ba7cde46
Bruno Tumelero <51979425+btumelero@users.noreply.github.com>
2019-11-18 00:31:35 -0300

    Add files via upload

---
+++ Mission/BossDodgerMissionController.cs
@@ -0,0 +1,12 @@
+﻿public class BossDodgerMissionController : BossMissionController {
+
+  protected override void Start () {
+    base.Start();
+    timer.baseTime = 1.25f;
+    spawnChance[(byte) enemyType.ATTACKER] = 10;
+    spawnChance[(byte) enemyType.DEFENDER] = 10;
+    spawnChance[(byte) enemyType.DODGER] = 50;
+    spawnChance[(byte) enemyType.NORMAL] = 30;
+  }
+
+}
