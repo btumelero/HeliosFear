9ce2858563a5d34015d4e91e94d9226932158c3c
Bruno Tumelero <51979425+btumelero@users.noreply.github.com>
2019-12-07 18:09:45 -0300

    Add files via upload

---
+++ Controllers/Other/KamikazeController.cs
@@ -0,0 +1,39 @@
+﻿using Extensions;
+using UnityEngine;
+
+public class KamikazeController : MonoBehaviour {
+
+  #region Variáveis
+
+  private static readonly float velocity = 450f;
+  private static Vector3 screenCenter = Vector3.zero;
+
+  #endregion
+
+  #region Métodos da Unity
+
+  /**
+   * Start is called before the first frame update
+   * 
+   */
+  private void Start () {
+    Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+    rigidbody.setVelocity(
+      (screenCenter - transform.position).normalized,
+      velocity * Time.fixedDeltaTime);
+    rigidbody.fixRotation();
+  }
+
+  private void OnTriggerEnter (Collider other) {
+    bool hitPlayer = other.tag.Contains(Enums.Tags.Player.ToString());
+    if (hitPlayer) {
+      other.transform.root.GetComponent<LifeController>().shield--;
+    }
+    if (hitPlayer || other.tag.Equals(Enums.Tags.FriendlyBullet.ToString())) {
+      Destroy(gameObject);
+    }
+  }
+
+  #endregion
+
+}
