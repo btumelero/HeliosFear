50f46ea8454fd2474dce1445a06156ed81c2054d
Bruno Tumelero <51979425+btumelero@users.noreply.github.com>
2019-12-07 18:01:49 -0300

    Add files via upload

---
+++ Utils/Delegates/Delegates.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates {
+
+  public delegate void MovementType ();
+
+  public delegate void MissionStage ();
+
+  public delegate void AttackType ();
+
+  public delegate void RespawnType ();
+}
