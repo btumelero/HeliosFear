53fd1f2621c84d936b1af85f41e35968317248bc
Bruno Tumelero <51979425+btumelero@users.noreply.github.com>
2019-12-07 18:01:06 -0300

    Add files via upload

---
+++ Utils/Enums/Enums.cs
@@ -0,0 +1,61 @@
+﻿namespace Enums {
+
+  public enum Spaceships : byte {
+    Attacker, Defender, Dodger, Normal
+  }
+
+  public enum Achievement : byte {
+    AdvancedEngineUnlocked,
+    SpecialEngineUnlocked,
+    AdvancedShieldUnlocked,
+    SpecialShieldUnlocked,
+    AdvancedWeaponUnlocked,
+    SpecialWeaponUnlocked
+  }
+
+  public enum Player : byte {
+    Score, HighScore
+  }
+
+  public enum Tags : byte {
+    ShieldSlider, 
+    SpeedSlider, 
+    WeaponSlider, 
+    RemainingHpSlider, 
+    RemainingShieldSlider, 
+    Player,
+    Enemy,
+    PlayerShield, 
+    EnemyShield,
+    EnemyBullet,
+    FriendlyBullet,
+    Canvas,
+    Bullet,
+    Edges,
+    Boss,
+    BlackSides,
+    SpawningZone
+  }
+
+  public enum Scenes : byte {
+    MainMenu, SpaceshipCustomization, SpaceshipSelection, Battle, BossSelection
+  }
+
+  public enum Input : byte {
+    Fire1, Shield, Speed, Weapon, Horizontal, Vertical
+  }
+
+  public enum Customization : byte { 
+    Basic = 1, Advanced = 2, Special = 3 
+  }
+
+  public enum Movement : byte { 
+    Leftward, Rightward, Downward, Halted
+  }
+
+  public enum Missions : byte {
+    Attacker, Defender, Dodger, Survival, Tutorial
+  }
+
+}
+
