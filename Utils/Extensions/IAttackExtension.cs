70130e7fab65fe9918e57048f306832f7544dcbf
Bruno Tumelero <b.tumelero@hotmail.com>
2020-02-02 00:22:09 +0000

    GITPOD 9

--- Utils/Extensions/IAttackExtension.cs
+++ Utils/Extensions/IAttackExtension.cs
@@ -1,15 +1,15 @@
-﻿using Interfaces;
+using Interfaces;
 using UnityEngine;
 
 namespace Extensions {
 
-  public static class ISpecialAttackExtension {
+  public static class IAttackExtension {
 
     /**
      * Calcula o ângulo em que o tiro deve ser disparado e 
      * transforma o resultado em um vetor com valores de 0~1 pra unity poder usar o vetor
      */
-    public static Vector3 getPlayerDirection (this ISpecialAttack attack, Transform shooter) {
+    public static Vector3 getPlayerDirection (this IAttack attack, Transform shooter) {
       return Mission.spaceship != null ?
         (Mission.spaceship.transform.position - shooter.position).normalized
         :
